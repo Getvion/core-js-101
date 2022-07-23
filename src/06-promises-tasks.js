95cc63b9736b6061f42f08244951bb361f02caf7
Oleg Somov <gog0125@mail.ru>
2022-07-23 13:23:57 +0300

    feat: add solutions for promises, objects, dates

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,20 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
-}
+function willYouMarryMe(isPositiveAnswer) {
+  if (isPositiveAnswer === true) {
+    return new Promise((resolve) => resolve('Hooray!!! She said "Yes"!'));
+  }
 
+  if (isPositiveAnswer === false) {
+    return new Promise((resolve) => resolve('Oh no, she said "No".'));
+  }
+
+  return new Promise((_, rej) => {
+    const message = 'Wrong parameter is passed! Ask her again.';
+    return rej(new Error(message));
+  });
+}
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +57,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +80,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +101,11 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+async function chainPromises(array, action) {
+  const promisesValues = [];
+  await array.forEach((item) => item.then((data) => promisesValues.push(data)));
+
+  return promisesValues.reduce(action);
 }
 
 module.exports = {
